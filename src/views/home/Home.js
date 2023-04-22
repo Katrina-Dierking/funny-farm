78c3890604b781dea2df2c1ccf9e7c26ceaaae90
Katrina <kalynndier@gmail.com>
2023-04-22 12:41:21 -0400

    dot background

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -4,9 +4,9 @@ import './home.scss'
 function Home() {
   return (
     <section className="home">
-      <h1>Welcome to the Funny Farm</h1>
+      <div className="homeHead"></div>
       <section className="top">
-        TOP
+        <h1>Welcome to the Funny Farm</h1>
         <div className="wave">
           <svg
             data-name="Layer 1"
@@ -23,36 +23,12 @@ function Home() {
       </section>
       <section className="middle">
         MIDDLE
-        <div className="wave midWave">
-          <svg
-            data-name="Layer 1"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 1200 120"
-            preserveAspectRatio="none"
-          >
-            <path
-              d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-              class="midWave"
-            ></path>
-          </svg>
-        </div>
         <div class="spacer layer1"></div>
       </section>
       <section className="bottom">
         BOTTOM
-        <div className="wave">
-          <svg
-            data-name="Layer 1"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 1200 120"
-            preserveAspectRatio="none"
-          >
-            <path
-              d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-              class="shape-fill"
-            ></path>
-          </svg>
-        </div>
+
+         
       </section>
     </section>
   );
